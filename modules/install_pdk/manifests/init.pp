9db58a33074e05798da1239ed8de514cbd10716e
Evan Burt <eburt.usmc+github@gmail.com>
2017-11-04 14:51:15 -0400

    Added Darwin and default support.

--- modules/install_pdk/manifests/init.pp
+++ modules/install_pdk/manifests/init.pp
@@ -1,7 +1,19 @@
 class install_pdk {
-  package { 'pdk_1.2.1.0-1xenial_amd64' :
-    ensure   => present,
-    source   => 'puppet://modules/install_pdk/pdk_1.2.1.0-1xenial_amd64.deb',
-    provider => 'dpkg',
+  case $facts['osfamily'] {
+    'Linux' : {
+      package { 'pdk_1.2.1.0-1xenial_amd64' :
+        ensure   => present,
+        source   => 'puppet://modules/install_pdk/pdk_1.2.1.0-1xenial_amd64.deb',
+        provider => 'dpkg',
+      }
+    }
+    'Darwin' : {
+      package { 'pdk-1.2.1.0-1.osx10.12' :
+        ensure   => present,
+        source   => 'puppet:///modules/install_pdk/pdk-1.2.1.0-1.osx10.12.dmg',
+        provider => 'pkgdmg',
+      }
+    }
+    default : {}
   }
 }
